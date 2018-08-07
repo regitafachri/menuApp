eedbf3f8acd5060c50e574c25c8d49c6964a28f7
regita fachri septiani <regitafach@MacBook-Pro-regita.local>
2018-08-07 20:01:32 +0700

    Add component header

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 
 const API_URL =
   "https://ace.tokopedia.com/search/v2.6/product?shop_id=345986&ob=9&rows=80&start=0&full_domain=www.tokopedia.com&scheme=https&device=desktop&source=shop_product";
 
+  // Class component or statefull component
 export default class App extends Component {
   render() {
     return <View/>;
